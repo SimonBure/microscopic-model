d99ea1cfc9693e3adc319f67cb11af75e58188c1
simon <simon.bure@mailo.com>
2024-11-12 23:13:18 +0100

    Creating 3 screens, one for each particle type

--- simulation.py
+++ simulation.py
@@ -16,8 +16,13 @@ if __name__ == "__main__":
     diffusion_coef = 1.0
     attraction_coef = 100.0
 
-    width, height = 500, 500
+    width = 2100
+    height = width // 3
     screen = pygame.display.set_mode((width, height), flags=pygame.SCALED)
+    screen_plants = pygame.surface.Surface((width // 3 - 33, height))
+    screen_surface_water = pygame.surface.Surface((width // 3 - 33, height))
+    screen_soil_water = pygame.surface.Surface((width // 3 - 33, height))
+
 
     # Time parameters
     time_step = 0.1
@@ -27,15 +32,15 @@ if __name__ == "__main__":
 
     is_running = True
 
-    nb_particles = 100
+    nb_particles = 300
     particles = [Particle(width * random.random(), height * random.random(), 2 * random.random() - 1,
                           2 * random.random() - 1, 3) for _ in range(nb_particles)]
 
-    plants = [Plant(width * random.random(), height * random.random(), 2 * random.random() - 1,
+    plants = [Plant((width // 3 - 33) * random.random(), height * random.random(), 2 * random.random() - 1,
                           2 * random.random() - 1, 3) for _ in range(nb_particles // 3)]
-    surface_waters = [SurfaceWater(width * random.random(), height * random.random(), 2 * random.random() - 1,
+    surface_waters = [SurfaceWater((width // 3 - 33) * random.random(), height * random.random(), 2 * random.random() - 1,
                           2 * random.random() - 1, 3) for _ in range(nb_particles // 3)]
-    soil_waters = [SoilWater(width * random.random(), height * random.random(), 2 * random.random() - 1,
+    soil_waters = [SoilWater((width // 3 - 33) * random.random(), height * random.random(), 2 * random.random() - 1,
                                    2 * random.random() - 1, 3) for _ in range(nb_particles // 3)]
 
     while is_running:
@@ -46,27 +51,32 @@ if __name__ == "__main__":
                 if event.key == pygame.K_ESCAPE:
                     is_running = False
 
-        screen.fill(pygame.color.Color("white"))
+        screen_plants.fill(pygame.color.Color("white"))
+        screen_surface_water.fill(pygame.color.Color("white"))
+        screen_soil_water.fill(pygame.color.Color("white"))
 
         pygame.display.set_caption(f"Arid Ecosystem Simulation\tFPS: {int(clock.get_fps())}")
 
         for p in plants:
-            p.draw(screen)
+            p.draw(screen_plants)
             p.x += (math.sqrt(time_step / 2) * brownian_motion() -
                     time_step * attraction_coef * p.attraction_along_x(particles) / nb_particles)
             p.y += (math.sqrt(time_step / 2) * brownian_motion() -
                     time_step * attraction_coef * p.attraction_along_y(particles) / nb_particles)
 
         for o in surface_waters:
-            o.draw(screen)
+            o.draw(screen_surface_water)
             o.x += math.sqrt(time_step / 2) * brownian_motion()
             o.y += math.sqrt(time_step / 2) * brownian_motion()
 
         for w in soil_waters:
-            w.draw(screen)
+            w.draw(screen_soil_water)
             w.x += math.sqrt(time_step / 2) * brownian_motion()
             w.y += math.sqrt(time_step / 2) * brownian_motion()
 
+        screen.blit(screen_plants, (0, 0))
+        screen.blit(screen_surface_water, (width // 3 - 33 + 50, 0))
+        screen.blit(screen_soil_water, (2 * (width // 3 - 33 + 50), 0))
         pygame.display.flip()
         clock.tick(60)  # limitation des fps à 60
 
